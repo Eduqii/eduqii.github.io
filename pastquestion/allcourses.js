00bed64132c23cacc8e1ea819f8c30b7ffab1c65
Dwamena Bernard Osei <solutionsbenit@gmail.com>
2021-03-17 16:55:20 +0000

    Update allcourses.js

--- pastquestion/allcourses.js
+++ pastquestion/allcourses.js
@@ -6,13 +6,13 @@
         return window.open('https://eduqii.org/allcourses/courses/maths/mathscalculator/index.php','_blank')
       }
  openWindowMathspastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/maths/mathspastquestions/index.php', '_blank')
+        return window.open('https://eduqii.org/allcourses/courses/maths/mathspastquestions/years/index.php', '_blank')
       }
 
 
    //Science
 openWindowSciencepastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/science/sciencepastquestions/index.php','_blank')
+        return window.open('https://eduqii.org/allcourses/courses/science/sciencepastquestions/years/index.php','_blank')
       }
 openWindowScientificCalc = e => {
         return window.location.assign('https://eduqii.org/allcourses/courses/science/scientificcalculator/index.php')
@@ -21,37 +21,34 @@ openWindowScientificCalc = e => {
 
 //Computing
 openWindowCompastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/computing/compastquestions/index.php','_blank')
+        return window.open('https://eduqii.org/allcourses/courses/computing/compastquestions/years/index.php','_blank')
       }
 // openWindowIctpastquestions = e => {
-   //     return window.location.assign('https://eduqii.org/allcourses/courses/maths/mathspastquestions/index.php')
+   //     return window.location.assign('https://eduqii.org/allcourses/courses/maths/mathspastquestions/years/index.php')
    //   }
 
 
  // English Language
  openWindowEnglishpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/index.php','_blank')
+        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/years/index.php','_blank')
       }     
- openWindowEnglishpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/index.php','_blank')
-      } 
 
       // Creative Art and Design
  openWindowCadpastquestions = e => {
    alert("No written exam for this subject yet");
       }     
  openWindowEnglishpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/index.php','_blank')
+        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/years/index.php','_blank')
       } 
 
 //French
 openWindowFrenchpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/french/frenchpastquestions/index.php','_blank')
+        return window.open('https://eduqii.org/allcourses/courses/french/frenchpastquestions/years/index.php','_blank')
       }
 
       //Religious & Moral Education
 openWindowRmepastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/rme/rmepastquestions/index.php','_blank')  
+        return window.open('https://eduqii.org/allcourses/courses/rme/rmepastquestions/years/index.php','_blank')  
       }
 
 //Physical and Health Education
@@ -71,9 +68,9 @@ openWindowOppastquestions = e => {
 
       //Social Studies
 openWindowsocialpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/socialstudies/sspastquestions/index.php','_blank') 
+        return window.open('https://eduqii.org/allcourses/courses/socialstudies/sspastquestions/years/index.php','_blank') 
       }
 
 // openWindowIctpastquestions = e => {
-   //     return window.location.assign('https://eduqii.org/allcourses/courses/maths/mathspastquestions/index.php')
+   //     return window.location.assign('https://eduqii.org/allcourses/courses/maths/mathspastquestions/years/index.php')
    //   }

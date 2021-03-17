70c42a2abae67f1c2fe31e6c667ab1cddff7f99c
Dwamena Bernard Osei <solutionsbenit@gmail.com>
2021-03-17 16:58:50 +0000

    Update allcourses.js

--- pastquestion/allcourses.js
+++ pastquestion/allcourses.js
@@ -3,34 +3,34 @@
 
 // Mathematics
  openWindowMaths = e => {
-        return window.open('https://eduqii.org/allcourses/courses/maths/mathscalculator/index.php','_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/maths/mathscalculator/index.php','_blank')
       }
  openWindowMathspastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/maths/mathspastquestions/years/index.php', '_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/maths/mathspastquestions/years/index.php', '_blank')
       }
 
 
    //Science
 openWindowSciencepastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/science/sciencepastquestions/years/index.php','_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/science/sciencepastquestions/years/index.php','_blank')
       }
 openWindowScientificCalc = e => {
-        return window.location.assign('https://eduqii.org/allcourses/courses/science/scientificcalculator/index.php')
+        return window.location.assign('https://eduqii.org/pastquestion/courses/science/scientificcalculator/index.php')
       }
 
 
 //Computing
 openWindowCompastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/computing/compastquestions/years/index.php','_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/computing/compastquestions/years/index.php','_blank')
       }
 // openWindowIctpastquestions = e => {
-   //     return window.location.assign('https://eduqii.org/allcourses/courses/maths/mathspastquestions/years/index.php')
+   //     return window.location.assign('https://eduqii.org/pastquestion/courses/maths/mathspastquestions/years/index.php')
    //   }
 
 
  // English Language
  openWindowEnglishpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/years/index.php','_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/english/englishpastquestions/years/index.php','_blank')
       }     
 
       // Creative Art and Design
@@ -38,17 +38,17 @@ openWindowCompastquestions = e => {
    alert("No written exam for this subject yet");
       }     
  openWindowEnglishpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/english/englishpastquestions/years/index.php','_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/english/englishpastquestions/years/index.php','_blank')
       } 
 
 //French
 openWindowFrenchpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/french/frenchpastquestions/years/index.php','_blank')
+        return window.open('https://eduqii.org/pastquestion/courses/french/frenchpastquestions/years/index.php','_blank')
       }
 
       //Religious & Moral Education
 openWindowRmepastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/rme/rmepastquestions/years/index.php','_blank')  
+        return window.open('https://eduqii.org/pastquestion/courses/rme/rmepastquestions/years/index.php','_blank')  
       }
 
 //Physical and Health Education
@@ -68,9 +68,9 @@ openWindowOppastquestions = e => {
 
       //Social Studies
 openWindowsocialpastquestions = e => {
-        return window.open('https://eduqii.org/allcourses/courses/socialstudies/sspastquestions/years/index.php','_blank') 
+        return window.open('https://eduqii.org/pastquestion/courses/socialstudies/sspastquestions/years/index.php','_blank') 
       }
 
 // openWindowIctpastquestions = e => {
-   //     return window.location.assign('https://eduqii.org/allcourses/courses/maths/mathspastquestions/years/index.php')
+   //     return window.location.assign('https://eduqii.org/pastquestion/courses/maths/mathspastquestions/years/index.php')
    //   }

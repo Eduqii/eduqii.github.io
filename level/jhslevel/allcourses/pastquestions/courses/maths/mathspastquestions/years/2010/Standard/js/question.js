4360b86f5cbe94d7c661cf5441c46b0296a6d0c0
Dwamena Bernard Hakeem <77701940+Eduqii@users.noreply.github.com>
2021-08-06 17:59:57 +0000

    refactor

--- level/jhslevel/allcourses/pastquestions/courses/maths/mathspastquestions/years/2010/Standard/js/question.js
+++ level/jhslevel/allcourses/pastquestions/courses/maths/mathspastquestions/years/2010/Standard/js/question.js
@@ -4,24 +4,24 @@
 const quiz = [
   {
 
-question: ' Human activities are generally controlled by',
+question: 'Which of the following sets is well defined?',
 
-options: [' technology',
- ' social environment',
- ' superstition',
- ' scientific research'],
-answer: 1
+options: ['{Man, Kofi, Red, 14}',
+ '{Ink, Mango, Green, Nail}',
+ '{Car, Road, Glass, Book}',
+ '{Seth, Mary, Jacob, Evelyn}'],
+answer: 3
 
 },
 
 { 
 
-question: ' The Kakum Forest in Ghana promotes economic development through its',
+question: 'If set B is a subset of set A, then',
 
-options: [' supply of firewood',
- ' supply of timber',
- ' suitable location',
- ' tourist attraction'],
+options: ['sets A and B have the same number of elements',
+ 'some members of set B can be found in set A',
+ 'no member of set B is in set A',
+ 'all the members of set B are in set A'],
 
 answer: 3
 
@@ -29,77 +29,12 @@ answer: 3
 
 { 
 
-question: ' Irresponsible adolescent behaviour usually results in',
+question: 'The least common multiple (LCM) of 16, 30 and 36 is',
 
-options: [' self-reliance',
- ' loss of dignity',
- ' broken homes',
- ' drug abuse'],
-
-answer: 1
-
-},
-
-{ 
-
-question: ' Ethnic groups in Ghana are distinctly identified by their',
-
-options: [' dance',
- ' music',
- ' language',
- ' food'],
-
-answer: 2
-
-},
-
-{ 
-
-question: ' The crop types which grow well in the Guinea Savanna are',
-
-options: [' millet and groundnuts',
- ' cocoa and rice',
- ' shallots and cassava',
- ' plantain and cola'],
-
-answer: 0
-
-},
-
-{ 
-
-question: ' The health needs of people can be improved by',
-
-options: [' building more schools',
- ' locating a hospital closer to the people',
- ' providing good drinking water',
- ' eating more carbohydrate foods'],
-
-answer: 2
-
-},
-
-{ 
-
-question: ' The British colonized parts of the Gold Coast largely through',
-
-options: [' conquest',
- ' persuasion',
- ' bribery',
- ' invitation'],
-
-answer: 0
-
-},
-
-{ 
-
-question: ' The outermost part of the earth is called the',
-
-options: [' core',
- ' mantle',
- ' atmosphere',
- ' crust'],
+options: ['3',
+ '6',
+ '240',
+ '720'],
 
 answer: 3
 
@@ -107,12 +42,77 @@ answer: 3
 
 { 
 
-question: ' The Europeans came to the Gold Coast purposely to',
+question: 'The sum of 5 and x divided by 4 is equal to 3.25. Find the value of x.',
 
-options: [' engage in trading activities',
- ' spread the gospel',
- ' explore the land',
- ' establish schools'],
+options: ['8',
+ '7',
+ '2<sup>1</sup>&frasl;<sub>4</sub>',
+ '-3<sup>4</sup>&frasl;<sub>13</sub>'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'The numbers 32, 33, 34, \u2026, \u2026, 42 form a sequence in base 5',
+
+options: [' 35, 36',
+ '30, 31',
+ '40, 41',
+ '31, 41'],
+
+answer: 2
+
+},
+
+{ 
+
+question: 'Write down all the integers in the set A = {-10, -4, 0, \xBC, 2\xBD, 45, 100}',
+
+options: ['{-10, -4, 0, 45, 100}',
+ '{-10, – 4}',
+ '{0, 45,100}',
+ '{	\xBC, 2	\xBD}'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'Find the total cost of 25 pens and 75 books if each pen costs GH\xA2 0.20 and each book costs GH\xA2 0.30.',
+
+options: ['GH\xA222.50',
+ 'GH\xA223.50',
+ 'GH\xA227.50',
+ 'GH\xA250.00'],
+
+answer: 2
+
+},
+
+{ 
+
+question: 'Simplify – 27 + 18 – (10 – 14) – (-2)',
+
+options: ['-3',
+ '-7',
+ '-11',
+ '-35'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'Arrange the following numbers from the lowest to the highest: 0.5, 3, -5, 0.',
+
+options: ['0, 0.5, -5, 3',
+ '0, -5, 0.5, 3',
+ '-5, 0, 0.5, 3',
+ '-5, 0.5, 0, 3'],
 
 answer: 2 
 
@@ -120,141 +120,12 @@ answer: 2
 
 { 
 
-question: ' The adoption of modern cultural practices enables a society to',
+question: 'Find how many pieces of cloth 5\xBD m long that can be cut from a roll of cloth 121 m long.',
 
-options: [' develop economically',
- ' lead good moral lives',
- ' abandon its traditional values',
- ' respect individual human rights'],
-
-answer: 0
-
-},
-
-{ 
-
-question: ' The use of orthodox and traditional medicine for the treatment of the same disease is an example of',
-
-options: [' outmoded culture',
- ' cultural lag',
- ' parallel culture',
- ' cultural change'],
-
-answer: 2
-
-},
-
-{ 
-
-question: ' Water bodies on a physical map are represented by',
-
-options: [' blue colour',
- ' green colour',
- ' yellow colour',
- ' brown colour'],
-
-answer: 0
-
-},
-
-{ 
-
-question: ' The earth experiences equal day and equal night each year on',
-
-options: [' 21st March and 23rd September',
- ' 21st June and 23rd September',
- ' 21st June and 22nd December',
- ' 22nd March and 22nd December'],
-
-answer: 0
-
-},
-
-{ 
-
-question: ' Which of the following is a regional capital in Northern Ghana',
-
-options: [' Navrongo',
- ' Wa',
- ' Bawku',
- ' Yendi'],
-answer: 1
-
-},
-
-{ 
-
-question: ' Which of the following towns is noted for the production of gold in Ghana?',
-
-options: [' Awaso',
- ' Nsuta',
- ' Prestea',
- ' Akwatia'],
-
-answer: 2
-
-},
-
-{ 
-
-question: ' Free vaccines used for immunization in Ghana are supplied by the',
-
-options: [' International Labour Organization',
- ' World Health Organization',
- ' Ministry of Health',
- ' National Health Insurance Scheme'],
-
-answer: 1
-
-},
-
-{ 
-
-question: 'The main effect of the Trans-Atlantic Slave Trade is the reduction of',
-
-options: [' capital resource',
- ' human resource',
- ' natural resource',
- ' mineral resource'],
-
-answer: 1
-
-},
-
-{ 
-
-question: ' Governor Gordon Guggisberg is known to have established the',
-
-options: [' Korle-Bu Hospital',
- ' University of Ghana',
- ' Mfantsipim School',
- ' Cocoa Marketing Board'],
-
-answer: 1
-
-},
-
-{ 
-
-question: ' The main institution that helps the Executive arm of government to implement its policies is the',
-
-options: [' Civil Service',
- ' Judicial Service',
- ' Audit Service',
- ' Statistical Service'],
-
-answer: 0
-
-},
-
-{ 
-
-question: ' The West Coast of Africa is washed by the',
-
-options: [' Pacific Ocean',
- ' Antarctic Ocean',
- ' Indian Ocean',
- ' Atlantic Ocean'],
+options: ['665\xBD',
+ '115\xBD',
+ '66',
+ '22'],
 
 answer: 3
 
@@ -262,12 +133,25 @@ answer: 3
 
 { 
 
-question: ' Private businesses can contribute to the development of the nation by',
+question: 'Find the value of 124.3 + 0.275 + 74.06, correcting your answer to one decimal place.',
 
-options: [' registering their businesses',
- ' purchasing expensive machines',
- ' paying their taxes as required',
- ' producing expensive goods'],
+options: ['198.6',
+ '198.7',
+ '892.0',
+ '892.4'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'Esi and Kwasi are 12 and 8 years old respectively. They share 60 mangoes in the ratio of their ages. How many mangoes does Esi get?',
+
+options: ['42',
+ '40',
+ '36',
+ '18'],
 
 answer: 2
 
@@ -275,12 +159,135 @@ answer: 2
 
 { 
 
-question: ' Floods can best be controlled in our communities through',
+question: 'It takes 6 students 1 hour to sweep their school compound. How long will it take 15 students to sweep the same compound?',
 
-options: [' construction of boreholes',
- ' planned layout',
- ' harvesting of rainwater',
- ' tarring of roads'],
+options: ['24 minutes',
+ '12 minutes',
+ '3 hours',
+ '2 hours'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'A housing agent makes a commission of GH\xA2 103,500 when he sells a house for GH\xA2 690,000. Calculate the percentage of his commission.',
+
+options: ['15.0%',
+ '10.0%',
+ '7.5%',
+ '5.0%'],
+
+answer: 0
+
+},
+{ 
+
+question: ' A simple interest of GH\xA2 37,500.00 is earned on an amount of GH\xA2 500,000.00 for 3 years. Find the rate of interest per annum.',
+
+options: ['20.0%',
+ '10.0%',
+ '5.0%',
+ '2.5%'],
+answer: 3
+
+},
+
+
+{ 
+
+question: 'Simplify: (8x<sup>2</sup>y<sup>3</sup>)(<sup>3</sup>&frasl;<sub>8</sub>xy<sup>4</sup>)',
+
+options: ['3x<sup>3</sup>y<sup>7</sup>',
+ '3x<sup>2</sup>y<sup>7</sup>',
+ '3x<sup>3</sup>y<sup>4</sup>',
+ '3xy'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'The scores of 10 students in an examination are given as follows:' + '<br />' +
+'45, 12, 75, 81, 54, 51, 24, 67, 19 and 39.' + '<br />' +
+'What is the median of the scores?',
+
+options: ['39',
+ '48',
+ '51',
+ '54'],
+
+answer: 1
+
+},
+
+{ 
+
+question: 'A pie chart is to be drawn from the data in the following table:' + '<br />' +
+'<table>' + '<tr>' + '<td>' + 'Cassava' + '</td>' + '<td>' + '20%' + '</td>' + '</tr>' +
+'<tr>' + '<td>' + 'Yam' + '</td>' + '<td>' + '17%' + '</td>' + '</tr>' + 
+'<tr>' + '<td>' + 'Plantain' + '</td>' + '<td>' + '28%' + '</td>' + '</tr>' + 
+'<tr>' + '<td>' + 'Maize' + '</td>' + + '<td>' + '35%' + '</td>' + '</tr>' + '</table>' + '<br />' + '<br />' +
+'What will be the value of the angle of the sector for maize?',
+
+options: ['126.0\xBA',
+ '100.8\xBA',
+ '72.0\xBA',
+ '61.2\xBA'],
+
+answer: 0
+
+},
+
+{ 
+
+question: 'Eighteen cards are numbered from 11 to 29. If one card is chosen at random, what is the probability that it contains the digit 2?',
+
+options: ['<sup>3</sup>&frasl;<sub>9</sub>',
+ '<sup>5</sup>&frasl;<sub>18</sub>',
+ '<sup>5</sup>&frasl;<sub>9</sub>',
+ '<sup>11</sup>&frasl;<sub>19</sub>'],
+
+answer: 3
+
+},
+
+{ 
+
+question: 'Find the value of x, if <sup>x</sup>&frasl;<sub>4</sub> + 1 = 5.',
+
+options: ['24',
+ '20',
+ '19',
+ '16'],
+
+answer: 3
+
+},
+
+{ 
+
+question: 'Factorize: xy + 5x + 2y + 10',
+
+options: ['(x + 5)(2y + 10)',
+ '(x + 2)(y + 10)',
+ '(x + 5)(y + 2)',
+ '(x + 2)(y + 5)'],
+
+answer: 3
+
+},
+
+{ 
+
+question: 'If x \u2208 {2, 3, 4, 5}, find the truth set of 2x + 1 	\x3C 8',
+
+options: ['{2,3,4}',
+ '{2,3}',
+ '{3,4}',
+ '{4,5}'],
 
 answer: 1 
 
@@ -288,12 +295,12 @@ answer: 1
 
 { 
 
-question: ' One effect of colonization is that it made Ghanaians to',
+question: 'Solve the inequality: 7x – (10x + 3) \u2265 -9',
 
-options: [' produce more food crops',
- ' have one common language',
- ' be self-reliant economically',
- ' develop strong taste for foreign goods'],
+options: ['x \u2265 2',
+ 'x \u2264 4',
+ 'x \u2265 4',
+ 'x \u2264 2'],
 
 answer: 3
 
@@ -301,63 +308,25 @@ answer: 3
 
 { 
 
-question: ' Ghana co-operates with other countries to',
+question: 'Find the rule of the mapping: ',
 
-options: [' foster friendly relationships',
- ' increase her level of tax revenue',
- ' adopt a common code of conduct',
- ' create more jobs for her citizens'],
+options: ['x \u2192 4x - 3',
+ 'x \u2192 3 - 4x',
+ 'x \u2192 4x + 3',
+ 'x \u2192 4x + 5'],
 
-answer: 0
+answer: 2,
+img:'img/mapping.jpg'
 },
 
 { 
 
-question: ' The functions of a presiding member of a District Assembly are likened to those of the',
+question: 'Find the circumference of a circle whose area is equal to 64 \u03C0 cm<sup>2</sup>.',
 
-options: [' Clerk of Parliament',
- ' Chief Justice',
- ' Speaker of Parliament',
- ' Appeal Court Judge'],
-
-answer: 2
-
-},
-
-{ 
-
-question: ' The world is divided into Northern and Southern Hemispheres by the',
-
-options: [' Tropic of Cancer',
- ' Tropic of Capricorn',
- ' Equator',
- ' Greenwich Meridian'],
-
-answer: 2
-
-},
-
-{ 
-
-question: ' Relief rainfall is also known as',
-
-options: [' convectional rainfall',
- ' frontal rainfall',
- ' orographic rainfall',
- ' cyclonic rainfall'],
-
-answer: 2
-
-},
-
-{ 
-
-question: 'Head of State in Ghana from 1957 to 1960 was',
-
-options: [' Kwame Nkrumah',
- ' Queen of Britain',
- ' K. Busia',
- ' Gordon Guggisberg'],
+options: ['32 \u03C0 cm',
+ '16 \u03C0 cm',
+ '8 \u03C0 cm',
+ '4 \u03C0 cm'],
 
 answer: 1
 
@@ -365,14 +334,53 @@ answer: 1
 
 { 
 
-question: ' The Supreme Court of Ghana mainly',
+question: 'Which of the following geometric figures is the plane shape of a cube?',
 
-options: [' runs courses for Judges and Magistrates',
- ' remands criminals in prison custody',
- ' interprets the constitution and laws for the country',
- ' makes and implements laws in the country'],
+options: ['Circle',
+ 'Rectangle',
+ 'Square',
+ 'Triangle'],
 
 answer: 2
+
+},
+
+{ 
+
+question: 'How many lines of symmetry has a rectangle?',
+
+options: ['4',
+ '3',
+ '2',
+ '1'],
+
+answer: 2
+
+},
+
+{ 
+
+question: 'A rectangular box has length 20 cm, width 6 cm and height 4 cm. Find how many cubes of side 2 cm that will fit into the box.',
+
+options: ['120',
+ '60',
+ '30',
+ '15'],
+
+answer: 1
+
+},
+
+{ 
+
+question: 'The interior angle of a regular polygon is 120\xBA. How many sides has this polygon?',
+
+options: ['3',
+ '4',
+ '5',
+ '6'],
+
+answer: 3
  
 
 
@@ -380,45 +388,47 @@ answer: 2
 
 { 
 
-question: ' Daboya is noted for the mining of',
+question: 'In the diagram below, length of PS = length of SQ and angle SQR = 112\xBA. Find the value of x.',
 
-options: [' salt',
- ' gold',
- ' diamond',
- ' bauxite'],
+options: ['68\xBA',
+ '56\xBA',
+ '46\xBA',
+ '44\xBA'],
 
-answer: 1
-
-},
-
-{ 
-
-question: ' Which of the following is the best way of conserving Ghana’s forest cover?',
-
-options: [' Promoting the use of charcoal',
- ' Vigilance of forest guards',
- ' Encouraging the replanting of trees',
- ' Growing more cover crops'],
-
-answer: 2
+answer: 3,
+img:'triangle.png'
 
 },
 
 { 
 
-question: ' If a distance of 40 kilometres on land is represented by 10 centimetres on a map, what is the scale of the map?',
+question: 'XYZ is a right-angled triangle with length of sides as shown.' + '<br />' + 'Which of the following equations gives the value of z<sup>2</sup>?',
 
-options: [' 1 centimetre to 2 kilometres',
- ' 1 centimetre to 4 kilometres',
- ' 1 centimetre to 5 kilometres',
- ' 1 centimetre to 8 kilometres'],
+options: [' z<sup>2</sup> = (x<sup>2</sup> + y<sup>2</sup>)',
+ 'z<sup>2</sup> = (x – y)',
+ 'z<sup>2</sup> = (y<sup>2</sup> – x<sup>2</sup>)',
+ 'z<sup>2</sup> = (x<sup>2</sup> – y<sup>2</sup>)'],
 
-answer: 1
+answer: 2,
+img:'triangle2.png'
+
 },
 
 { 
 
-question: ' Slaves who were sent to the West Indies worked on',
+question: 'Express 7 min. 30 sec. as a percentage of 1 hour.',
+
+options: [' 2.5%',
+ ' 7.5%',
+ '11.7%',
+ '12.5%'],
+
+answer: 3
+},
+
+{ 
+
+question: 'The point (4,5) is translated to the point (3,1). What is the translation vector?',
 
 options: [' coconut plantations',
  ' pineapple plantations',

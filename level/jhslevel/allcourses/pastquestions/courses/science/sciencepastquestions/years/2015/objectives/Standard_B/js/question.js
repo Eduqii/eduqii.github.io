3ad39da20c0d979234f1b07acb5320d51b94fb8b
Dwamena Bernard Hakeem <77701940+Eduqii@users.noreply.github.com>
2021-08-19 16:46:44 +0000

    refactor

---
+++ level/jhslevel/allcourses/pastquestions/courses/science/sciencepastquestions/years/2015/objectives/Standard_B/js/question.js
@@ -0,0 +1,334 @@
+
+
+//Array of Objects
+const quiz = [
+  {
+		question: 'Brass is an alloy of',
+options: ['iron and carbon',
+ 'zinc and copper',
+ 'iron and copper',
+ 'copper and tin'],
+answer: 1
+},
+{
+question: 'An example of inorganic fertilizer is',
+options: ['ammonium nitrate',
+ 'cow dung',
+ 'farmyard manure',
+ 'poultry dropping'],
+answer: 0
+},
+{
+question: 'The component of the human blood which transports oxygen to all parts of the body is',
+options: ['plasma',
+ 'platelets',
+ 'red blood cells',
+ 'white blood cells'],
+answer: 2
+},
+{
+question: 'The importance of the fuse in an electrical circuit is to',
+options: ['regulate the voltage',
+ 'prevent damage to electrical appliances',
+ 'alter the flow of current in the circuit',
+ 'minimize the use of current'],
+answer: 1
+},
+{
+question: 'Which of the following step(s) is/are required in the scientific method?' + '<br />' + '<br />' + 
+'I.    Formulation of hypothesis' + '<br />' + 
+'II.    Identification of the problem' + '<br />' + 
+'III.    Experimentation',
+options: ['I only',
+ 'II only',
+ 'II and III only',
+ 'I, II and III'],
+answer: 3
+},
+{
+question: 'Which of the following food items produces amino acids as end product of digestion?',
+options: ['Cabbage',
+ 'Fish',
+ 'Margarine',
+ 'Rice'],
+answer: 1
+},
+{
+question: 'The process of increasing the strength of a signal using a transistor is known as',
+options: ['amplification',
+ 'biasing',
+ 'doping',
+ 'switching'],
+answer: 0
+},
+{
+question: 'The physical arrangement of soil particles into aggregates is termed',
+options: ['soil porosity',
+ 'soil profile',
+ 'soil structure',
+ 'soil texture'],
+answer: 1
+},
+{
+question: 'The solvent which is most effective in washing bitumen from the hand is',
+options: ['acid',
+ 'alcohol',
+ 'kerosene',
+ 'water'],
+answer: 2
+},
+{
+question: 'A piece of stone could be classified as an opaque material because it',
+options: ['absorbs all the light incident on it',
+ 'does not absorb light incident on it',
+ 'allows all the light incident on it to pass through it',
+ 'does not allow light incident on it to pass through it'],
+answer: 3
+},
+{
+question: 'Fish swims in water with little resistance because it possesses',
+options: ['gills',
+ 'scales',
+ 'caudal fins',
+ 'streamlined body'],
+answer: 3
+},
+{
+question: 'Which of the following conditions promote(s) rusting of iron?' + '<br />' + '<br />' + 
+'I.    Air' + '<br />' + 
+'II.    Moisture' + '<br />' +  
+'III.    Oil',
+options: ['I only',
+ 'II only',
+ 'I and II only',
+ 'II and III only'],
+answer: 2
+},
+{
+question: 'The change in the volume of water when a piece of stone is dropped into it is equal to the',
+options: ['density of the stone',
+ 'mass of the stone',
+ 'volume of the stone',
+ 'weight of the stone'],
+answer: 2
+},
+{
+question: 'The second stage in the life cycle of a mosquito is the',
+options: ['egg',
+ 'imago',
+ 'larva',
+ 'pupa'],
+answer: 2
+},
+{
+question: 'Which of the following objects could be attracted by a magnet?',
+options: ['Copper chain',
+ 'Gold chain',
+ 'Steel blade',
+ 'Aluminium spoon'],
+answer: 2
+},
+{
+question: 'The knowledge of soil texture is important because it',
+options: ['influences plant population',
+ 'determines the planting distance',
+ 'determines the type of crop to be grown',
+ 'influences the method of pest control'],
+answer: 2
+},
+{
+question: 'How many atoms are present in CaCl' + '<sub>' + '2' + '</sub>' + '?',
+options: ['2',
+ '3',
+ '4',
+ '5'],
+answer: 1
+},
+{
+question: 'The transfer of heat from the bottom to the top of a beaker containing water is by',
+options: ['absorption',
+ 'conduction',
+ 'convection',
+ 'radiation'],
+answer: 2
+},
+{
+question: 'An example of a chemical compound is',
+options: ['aluminium',
+ 'ammonia',
+ 'oxygen',
+ 'phosphorus'],
+answer: 1
+},
+{
+question: 'The type of the human teeth used for biting food substances is',
+options: ['canines',
+ 'incisors',
+ 'molars',
+ 'premolars'],
+answer: 1
+},
+{
+question: 'Which of the following heavenly bodies is a star?',
+options: ['Jupiter',
+ 'Moon',
+ 'Sun',
+ 'Venus'],
+answer: 2
+},
+{
+question: 'Which of the following organisms is an endoparasite?',
+options: ['Fleas',
+ 'Louse',
+ 'Tapeworm',
+ 'Tick'],
+answer: 2
+},
+{
+question: 'The part of the plant where pollination occurs is the',
+options: ['flowers',
+ 'leaves',
+ 'roots',
+ 'stems'],
+answer: 0
+},
+{
+question: 'Which of the following substances is capable of neutralizing an acid?',
+options: ['Vinegar',
+ 'Water',
+ 'Sodium chloride',
+ 'Sodium hydroxide'],
+answer: 3
+},
+{
+question: 'Droplet infection is the method by which',
+options: ['diseases are transmitted from animals to plants',
+ 'infected animals are processed',
+ 'human beings protect themselves from diseases',
+ 'diseases are spread through the air'],
+answer: 3
+},
+{
+question: 'An example of a derived quantity is',
+options: ['length',
+ 'mass',
+ 'time',
+ 'velocity'],
+answer: 3
+},
+{
+question: 'A structure in the human reproductive system which stores sperms temporarily is the',
+options: ['epididymis',
+ 'penis',
+ 'scrotum',
+ 'vulva'],
+answer: 0
+},
+{
+question: 'Which of the following statements about diffusion is/are correct?' + '<br />' + '<br />' + 
+'I.    It involves the movement of water molecules only' +  '<br />' + 
+'II.    It occurs in both gases and liquids' + '<br />'  + 
+'III.    It involves a semi-permeable membrane',
+options: ['I only',
+ 'II only',
+ 'I and II only',
+ 'I, II and III'],
+answer: 1
+},
+{
+question: 'A farming system which requires at least three years programme is',
+options: ['mixed cropping',
+ 'crop rotation',
+ 'organic farming',
+ 'mixed farming'],
+answer: 1
+},
+{
+question: 'The component of a living cell responsible for respiration is',
+options: ['chloroplast',
+ 'mitochondria',
+ 'nucleus',
+ 'vacuole'],
+answer: 1
+},
+{
+question: 'Which of the following statements about a second class lever is correct? The',
+options: ['pivot is between the load and the effort',
+ 'pivot and the load are at the same position',
+ 'load is between the effort and the pivot',
+ 'effort is  between the load and the pivot'],
+answer: 2
+},
+{
+question: 'One advantage of soft water over hard water is that soft water',
+options: ['has a pleasant taste',
+ 'can prevent heart diseases',
+ 'does not waste soap',
+ 'forms scales in kettles'],
+answer: 2
+},
+{
+question: 'The gas produced when glucose is oxidized during aerobic respiration is',
+options: ['hydrogen',
+ 'nitrogen',
+ 'oxygen',
+ 'carbon dioxide'],
+answer: 3
+},
+{
+question: 'The systematic name for N2O is',
+options: ['nitrogen (I) oxide',
+ 'nitrogen (II) oxide',
+ 'nitrogen (III) oxide',
+ 'nitrogen (IV) oxide'],
+answer: 3
+},
+{
+question: 'A husbandry practice which involves the removal of unwanted plant parts is known as',
+options: ['mulching',
+ 'pruning',
+ 'thinning',
+ 'pricking out'],
+answer: 1
+},
+{
+question: 'The number of p-n junctions in a transistor is',
+options: ['1',
+ '2',
+ '3',
+ '4'],
+answer: 1
+},
+{
+question: 'Fruits dispersed by wind',
+options: ['have hairs',
+ 'are sticky',
+ 'are succulent',
+ 'have hooks'],
+answer: 0
+},
+{
+question: 'Substances that burn living tissues when they come into contact with these tissues are considered',
+options: ['corrosive',
+ 'flammable',
+ 'irritant',
+ 'toxic'],
+answer: 0
+},
+{
+question: 'The function of the platelets in the circulatory system of humans is to',
+options: ['transport oxygen',
+ 'transport carbon dioxide',
+ 'clot the blood',
+ 'defend the body'],
+answer: 2
+},
+{
+question: 'Energy is the',
+options: ['force to move an object',
+ 'weight to do work',
+ 'ability to do work',
+ 'power to carry an object'],
+answer: 2
+}
+]

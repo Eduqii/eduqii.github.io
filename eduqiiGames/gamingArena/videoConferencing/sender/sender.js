8d81abd6169d822dbff164ae35356f414e4b869f
Dwamena Bernard Hakeem <77701940+Eduqii@users.noreply.github.com>
2021-08-25 11:37:37 +0000

    refactor

--- eduqiiGames/gamingArena/videoConferencing/sender/sender.js
+++ eduqiiGames/gamingArena/videoConferencing/sender/sender.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("ws://SERVER-IP-HERE:3000")
+const webSocket = new WebSocket("ws://192.30.252.0/22:443")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))

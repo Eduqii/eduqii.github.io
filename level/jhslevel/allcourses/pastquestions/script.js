ee0f3652f6ad34d459292447cdd49e817015cc58
Dwamena Bernard Hakeem <77701940+Eduqii@users.noreply.github.com>
2021-08-29 13:38:14 +0000

    refactor

--- level/jhslevel/allcourses/pastquestions/script.js
+++ level/jhslevel/allcourses/pastquestions/script.js
@@ -1,10 +1,10 @@
 // get the element
-const visitorB = document.getElementById('loading')
+const visitorB = document.getElementById('junior_high')
 
 // always checking if the element is clicked, if so, do alert('hello')
-visitorB.addEventListener("load", () => {
+visitorB.addEventListener("click", () => {
   var visitors = document.getElementById('page_visits');
   visitors += 1;
   console.log(visitors);
-  document.write(visitors + 1);
+ document.getElementById('page_visits').innerHTML = visitors;
 });

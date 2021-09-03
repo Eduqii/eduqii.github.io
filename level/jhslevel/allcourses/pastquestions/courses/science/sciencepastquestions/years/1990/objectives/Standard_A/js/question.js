f59acacf7bb2a3ceb99d9f4c57558281ce892995
Dwamena Bernard Hakeem <77701940+Eduqii@users.noreply.github.com>
2021-09-03 21:09:16 +0000

    refactor

---
+++ level/jhslevel/allcourses/pastquestions/courses/science/sciencepastquestions/years/1990/objectives/Standard_A/js/question.js
@@ -0,0 +1,405 @@
+
+
+//Array of Objects
+const quiz = [
+ //Q1
+	{
+question: 'Which of the following is a liquid?',
+options: ['Iron',
+'Mercury',
+ 'Brass',
+ 'Gold',
+ 'Aluminium'],
+answer: 1
+},
+//Q2
+{
+question: 'The space occupied by matter is its',
+options: ['area',
+'length ',
+ 'radius ',
+ 'width',
+ 'volume'],
+answer: 4
+},
+//Q3
+{
+question: 'Which of the following processes could be used to obtain water from a salt solution?',
+options: ['Filtration',
+'Decantation ',
+ 'Evaporation ',
+ 'Distillation',
+ 'Sedimentation'],
+answer: 3
+},
+//Q4
+{
+question: 'The portion of air which is used in burning is',
+options: ['nitrogen',
+'water vapour',
+ 'oxygen',
+ 'carbon dioxide',
+ 'inert gas'],
+answer: 2
+},
+//Q5
+{
+question: 'Brine is a saturated solution of',
+options: ['sodium chloride and carbon dioxide',
+'calcium carbonate and water',
+ 'sodium chloride and water',
+ 'calcium carbonate and kerosene',
+ 'magnesium and water'],
+answer: 2
+},
+//Q6
+{
+question: 'The chemical symbol for iron is',
+options: ['I ',
+'K ',
+ 'Fe ',
+ 'Au ',
+ 'Cl'],
+answer: 2
+},
+//Q7
+{
+question: 'A torchlight derives its energy from',
+options: ['petrol',
+'kerosene ',
+ 'a magnet ',
+ 'gas oil',
+ 'a dry cell'],
+answer: 4
+},
+//Q8
+{
+question: 'A substance which cannot be broken down into simpler substances is called',
+options: ['a molecule ',
+'a compound ',
+ 'an element ',
+ 'a mixture',
+ 'an atom'],
+answer: 2
+},
+//Q9
+{
+question: 'What type of energy is lost when sweat evaporates from the body?',
+options: ['light energy',
+'mechanical energy',
+ 'chemical energy',
+ 'Heat energy',
+ 'Electrical energy'],
+answer: 3
+},
+//Q10
+{
+question: 'The process by which steam is changed into liquid is called',
+options: ['evaporation ',
+'condensation ',
+ 'filtration',
+ 'melting',
+ 'sublimation'],
+answer: 1
+},
+//Q11
+{
+question: 'Which of the following is not true about frictional force? It',
+options: ['increases motion',
+'opposes motion',
+ 'damages machines',
+ 'produces heat',
+ 'decreases efficiency of machines'],
+answer: 0
+},
+//Q12
+{
+question: 'The instrument used in measuring the speed of wind is called:',
+options: ['Thermometer',
+'Wind Vane ',
+ 'Photometer ',
+ 'Anemometer ',
+ 'Hygrometer'],
+answer: 3
+},
+//Q13
+{
+question: 'The force which comes into operation when a ball rolls on the ground is:',
+options: ['gravitational force',
+'frictional force',
+ 'magnetic force',
+ 'electrostatic force',
+ 'Force due to tension'],
+answer: 1
+},
+//Q14
+{
+question: 'Which of the following is true about a first class lever?',
+options: ['The load is between the effort and the pivot',
+'The pivot is between the effort and the load',
+ 'The effort is between the pivot and the load',
+ 'The load is on the pivot',
+ 'There is no pivot'],
+answer: 1
+},
+//Q15
+{
+question: 'In which of the following situations is work not done? When a',
+options: ['truck moves',
+'wheelbarrow is pushed',
+ 'fruit falls from a tree',
+ 'football is kicked',
+ 'book lies on a table'],
+answer: 4
+},
+//Q16
+{
+question: 'Which of these structures regulate the amount of light that enters the eye?',
+options: ['Retina',
+'Cornea',
+ 'Pupil',
+ 'Choroid',
+ 'Lens'],
+answer: 2
+},
+//Q17
+{
+question: 'All of the following are herbivores except',
+options: ['sheep',
+'dog',
+ 'goat',
+ 'grasscutter',
+ 'rabbit'],
+answer: 1
+},
+//Q18
+{
+question: 'The brain and the spinal cord are part of the',
+options: ['digestive system',
+'circulatory system',
+ 'respiratory system',
+ 'nervous system',
+ 'excretory system'],
+answer: 3
+},
+//Q19
+{
+question: 'Which of the following is used by green plants for respiration?',
+options: ['Carbon dioxide',
+'Oxygen',
+ 'Light',
+ 'Water',
+ 'Chlorophyll'],
+answer: 1
+},
+//Q20
+{
+question: 'A fish dies shortly after being removed from water. This is because it',
+options: ['cannot swim ',
+'is streamlined ',
+ 'is hot on land ',
+ 'has scales',
+ 'cannot respire'],
+answer: 4
+},
+//Q21
+{
+question: 'Which of the following habitats are you likely to find a tadpole?',
+options: ['Bush',
+'Seashore',
+ 'Pond',
+ 'Deep well',
+ 'Vegetable garden'],
+answer: 2
+},
+//Q22
+{
+question: 'Which of these is not a characteristic of all living things?',
+options: ['Transpiration',
+'Growth',
+ 'Reproduction',
+ 'Feeding',
+ 'Respiration'],
+answer: 0
+},
+//Q23
+{
+question: 'Plants with thorns are not usually eaten by animals. The thorns therefore',
+options: ['assist the plants to grow',
+'make the plant strong',
+ 'render the plant poisonous',
+ 'protect the plant',
+ 'make the plant unattractive'],
+answer: 3
+},
+//Q24
+{
+question: 'Which of the following soils is likely to be found in a water logged area?',
+options: ['Sand',
+'Clay',
+ 'Gravel',
+ 'Black soil',
+ 'loam'],
+answer: 1
+},
+//Q25
+{
+question: 'Which of the following soil organisms improve soil fertility?',
+options: ['Ant-lion',
+'Red ant',
+ 'Millipede',
+ 'Snail',
+ 'Earthworm'],
+answer: 4
+},
+//Q26
+{
+question: 'Which of the following is cultivated on plantation basis?',
+options: ['Okro',
+'Tomato ',
+ 'Rubber ',
+ 'Cassava ',
+ 'Maize'],
+answer: 2
+},
+//Q27
+{
+question: 'The shape of a child\u2019s nose may resemble that of the mother due to',
+options: ['heredity ',
+'similarity ',
+ 'reflection ',
+ 'familiarity ',
+ 'attraction'],
+answer: 0
+},
+//Q28
+{
+question: 'All the following plants reproduce sexually except',
+options: ['coconut',
+'sweet potato',
+ 'pawpaw',
+ 'apple',
+ 'pear'],
+answer: 1
+},
+//Q29
+{
+question: 'Which of the following is not a mammal?',
+options: ['Whale',
+'Bat', 
+ 'Cat ',
+ 'Hen',
+ 'Mouse'],
+answer: 3
+},
+//Q30
+{
+question: 'Plants lose water through a process called',
+options: ['photosynthesis',
+'fertilization ',
+ 'transpiration ',
+ 'respiration',
+ 'evaporation'],
+answer: 2
+},
+//Q31
+{
+question: 'Fertilized ovules develop into',
+options: ['fruits',
+'cotyledon',
+ 'plumule',
+ 'testa',
+ 'seeds'],
+answer: 4
+},
+//Q32
+{
+question: 'In mammals, sperms are produced in the',
+options: ['testis ',
+'ureter ',
+ 'penis ',
+ 'urethra ',
+ 'ovary'],
+answer: 0
+},
+//Q33
+{
+question: 'Starch is digested and absorbed into the blood as',
+options: ['soluble starch',
+'water',
+ 'glucose',
+ 'fat',
+ 'sucrose'],
+answer: 2
+},
+//Q34
+{
+question: 'Which of the following organs are connected by the ureter?',
+options: ['Liver and kidney',
+'Kidney and bladder ',
+ 'Bladder and heart ',
+ 'Intestines and anus ',
+ 'Lungs and bladder'],
+answer: 1
+},
+//Q35
+{
+question: 'A patient who coughs and spits out blood may be suffering from',
+options: ['asthma',
+'pneumonia',
+ 'measles',
+ 'tuberculosis',
+ 'influenza'],
+answer: 3
+},
+//Q36
+{
+question: 'All of the following promote Primary Health except',
+options: ['keeping the surroundings clean',
+'effective disposal of waste',
+ 'personal hygiene',
+ 'balanced diet',
+ 'bleaching the skin'],
+answer: 4
+},
+//Q37
+{
+question: 'The personnel who see to it that individuals who do not maintain hygienic surroundings are taken to court is the',
+options: ['police',
+'lawyer',
+ 'sanitary inspector',
+ 'court clerk',
+ 'prison officer'],
+answer: 2
+},
+//Q38
+{
+question: 'Which of the following statements is not true about the wet season',
+options: ['Bush fires are common',
+'Tree and weeds grow fast',
+ 'Small animals increase in number',
+ 'The weather is generally cool',
+ 'Food is in abundance'],
+answer: 0
+},
+//Q39
+{
+question: 'A mirror is used to direct the sun\u2019s rays onto the classroom wall by',
+options: ['radiation ',
+'reflection ',
+ 'conduction ',
+ 'revolution ',
+ 'convection'],
+answer: 1
+},
+//Q40
+{
+question: 'The sun and the planets form the',
+options: ['universe',
+'atmosphere',
+ 'milky way',
+ 'constellation',
+ 'solar system'],
+answer: 4
+}
+]

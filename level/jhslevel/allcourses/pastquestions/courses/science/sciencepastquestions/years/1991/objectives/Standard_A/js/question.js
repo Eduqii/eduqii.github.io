03f9bf02e858464eaae96ecfb40f54b95adc1914
Dwamena Bernard Hakeem <77701940+Eduqii@users.noreply.github.com>
2021-09-03 20:49:49 +0000

    rft

---
+++ level/jhslevel/allcourses/pastquestions/courses/science/sciencepastquestions/years/1991/objectives/Standard_A/js/question.js
@@ -0,0 +1,365 @@
+
+
+//Array of Objects
+const quiz = [
+  {
+		question: 'The digestion of carbohydrates starts in the',
+options: ['mouth',
+ 'throat',
+ 'stomach',
+ 'duodenum',
+ 'ileum'],
+answer: 0
+},
+{
+question: 'Photosynthesis in plants requires chlorophyll, sunlight, water and',
+options: ['oxygen',
+ 'carbondioxide',
+ 'steam',
+ 'hydrogen',
+ 'nitrogen'],
+answer: 1
+},
+{
+question: 'Which of the following objects is a good conductor of heat?',
+options: ['Wood ',
+ 'Rubber ',
+ 'Copper ',
+ 'Plastic ',
+ 'Wool'],
+answer: 2
+},
+{
+question: 'What is the work donewhen a forceof 2.5N moves through a distance of 4 m?',
+options: ['0.6 J ',
+ '1.5 J ',
+ '1.6 J ',
+ '6.5 J ',
+ '10.0 J'],
+answer: 4
+},
+{
+question: 'The person who studies the weather is',
+options: ['apilot',
+ 'a physician ',
+ 'an astronaut ',
+ 'an astrologer ',
+ 'meteorologist'],
+answer: 4
+},
+{
+question: 'A child suffering from Kwashiorkor lacks',
+options: ['protein',
+ 'fats and oils',
+ 'vitamins',
+ 'mineral salts',
+ 'carbohydrates'],
+answer: 0
+},
+{
+question: 'Which of the following substances can dissolve almost all solutes?',
+options: ['Kerosene ',
+ 'Palmoil ',
+ 'Water',
+ 'Turpentine',
+ 'Petrol'],
+answer: 2
+},
+{
+question: 'All of the following methods are used to preserve food in our homes except',
+options: ['smoking',
+ 'salting ',
+ 'boiling ',
+ 'canning ',
+ 'drying'],
+answer: 3
+},
+{
+question: 'The chemical symbol for Potassium is',
+options: ['Fe ',
+ 'K ',
+ 'P ',
+ 'Pb ',
+ 'Zn'],
+answer: 1
+},
+{
+question: ' The part of the banana plant used in reproduction is called',
+options: ['sucker ',
+ 'rhizome ',
+ 'stem',
+ 'tuber',
+ 'bulb'],
+answer: 0
+},
+{
+question: ' The process by which kerosene rises up the wick in a lantern is',
+options: ['evaporation ',
+ 'conduction ',
+ 'diffusion',
+ 'capillarity',
+ 'convection'],
+answer: 3
+},
+{
+question: ' Which of the following animals can live both in water and on land?',
+options: ['Mouse',
+ 'Rat',
+ 'Frog',
+ 'Grasscutter',
+ 'Mud fish'],
+answer: 2
+},
+{
+question: ' A bar magnet attracts all the following objects except',
+options: ['pins',
+ 'pair ofscissors',
+ 'nails',
+ 'razorblade',
+ 'glass'],
+answer: 4
+},
+{
+question: 'When an object moves from rough surface onto a smooth one with the same energy, its speed',
+options: ['increases',
+ 'decreases',
+ 'remains thesame',
+ 'becomes zero',
+ 'cannot be determined'],
+answer: 0
+},
+{
+question: ' How many states of matter do we have?',
+options: ['2',
+ '3',
+ '4',
+ '5',
+ '6'],
+answer: 2
+},
+{
+question: ' All of the following animals lay eggs except',
+options: ['fish',
+ 'lizard ',
+ 'rabbit ',
+ 'toad',
+ 'turkey'],
+answer: 2
+},
+{
+question: ' Which of these is not a sense organ?',
+options: ['Eye ',
+ 'Ear ',
+ 'Nose ',
+ 'Skin',
+ 'Heart'],
+answer: 4
+},
+{
+question: ' Which of the following processes is used to separate insoluble solids from liquids?',
+options: ['Distillation ',
+ 'Sublimation ',
+ 'Filtration',
+ 'Evaporation',
+ 'Sedimentation'],
+answer: 2
+},
+{
+question: ' An organism living on another organism and causing harm to it is referred to as a',
+options: ['host',
+ 'parasite ',
+ 'producer ',
+ 'consumer ',
+ 'vector'],
+answer: 1
+},
+{
+question: ' What sickness are you likely to be suffering from when you have blood in your urine?',
+options: ['Typhoid ',
+ 'Syphilis ',
+ 'Cholera ',
+ 'Bilharzia ',
+ 'Ulcer'],
+answer: 3
+},
+{
+question: ' Which of these is a vegetable crop?',
+options: ['Pineapple',
+ 'Potato',
+ 'Cabbage ',
+ 'Cassava ',
+ 'Mango'],
+answer: 2
+},
+{
+question: ' Insects can walk on the surface of water because of',
+options: ['diffusion ',
+ 'capillarity ',
+ 'viscosity ',
+ 'friction',
+ 'surface tension'],
+answer: 4
+},
+{
+question: ' When a substance is made up of the same kind of atoms it is called',
+options: ['a mixture',
+ 'a compound ',
+ 'an element ',
+ 'asolution',
+ 'asolvent'],
+answer: 2
+},
+{
+question: ' What do human beings obtain from food to enable them work?',
+options: ['Blood',
+ 'Energy',
+ 'Vitamins ',
+ 'Proteins ',
+ 'Minerals'],
+answer: 1
+},
+{
+question: ' Which of these instruments is used in measuring humidity?',
+options: ['Hygrometer ',
+ 'Thermometer ',
+ 'Photometer',
+ 'Potometer',
+ 'Anemometer'],
+answer: 0
+},
+{
+question: ' The part of the plant which is found in the soil is the',
+options: ['flower ',
+ 'stem ',
+ 'leaf',
+ 'root',
+ 'fruit'],
+answer: 3
+},
+{
+question: ' When salt is put into soups, it',
+options: ['melts',
+ 'sublimes ',
+ 'dissolves ',
+ 'evaporates ',
+ 'expands'],
+answer: 2
+},
+{
+question: ' The complete development of a human baby in the womb normally takes',
+options: ['five months',
+ 'six months',
+ 'seven months',
+ 'nine months',
+ 'eleven months'],
+answer: 3
+},
+{
+question: 'The washing away of the top soil by running water is called ',
+options: ['erosion',
+ 'rotation',
+ 'plantation ',
+ 'weathering ',
+ 'irrigation'],
+answer: 0
+},
+{
+question: ' All of the following are solids except',
+options: ['aluminium',
+ 'lead',
+ 'silver',
+ 'potassium',
+ 'mercury'],
+answer: 4
+},
+{
+question: ' The process by which living things increase in number is referred to as',
+options: ['growth',
+ 'development',
+ 'reproduction',
+ 'multiplication',
+ 'addition'],
+answer: 2
+},
+{
+question: ' Which of the following soil types can hold the least amount of water?',
+options: ['Loam ',
+ 'Clay ',
+ 'Silt',
+ 'Sand',
+ 'Sandy loam'],
+answer: 3
+},
+{
+question: ' When a piece of candle is heated, it changes from',
+options: ['liquid to gas',
+ 'liquid to flame ',
+ 'solid to flame ',
+ 'solid to liquid ',
+ 'solid to gas'],
+answer: 3
+},
+{
+question: ' Which of the following metals is used for making roofing sheets?',
+options: ['Zinc',
+ 'Silver',
+ 'Mercury',
+ 'Tin',
+ 'Aluminium'],
+answer: 4
+},
+{
+question: ' What is the name of the final product of protein digestion?',
+options: ['Glucose ',
+ 'Fattyacid ',
+ 'Amino acid ',
+ 'Glycerol',
+ 'Sucrose'],
+answer: 2
+},
+{
+question: ' The lion is a typical example of',
+options: ['a pet',
+ 'a carnivore ',
+ 'an omnivore ',
+ 'a herbivore ',
+ 'a parasite'],
+answer: 1
+},
+{
+question: ' A uniform mixture of two or more metals is called',
+options: ['asolute',
+ 'a colloid',
+ 'a compound ',
+ 'a suspension ',
+ 'an alloy'],
+answer: 4
+},
+{
+question: ' Which of the following activities illustrates distillation?',
+options: ['Preparation ofsoup',
+ 'Preparation ofakpeteshie',
+ 'Tappingof palmwine ',
+ 'Preparation ofstarch ',
+ 'Freezingof water'],
+answer: 1
+},
+{
+question: ' The hatching of eggs into chicken depends on constant supply of',
+options: ['water',
+ 'feed',
+ 'pressure',
+ 'heat',
+ 'light'],
+answer: 3
+},
+{
+question: ' A fertilized ovule results in the formation of',
+options: ['an ovary',
+ 'a style ',
+ 'a pistil ',
+ 'a stigma',
+ 'an embryo'],
+answer: 4 
+} 
+]

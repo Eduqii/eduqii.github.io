ee553eff0e19e94e781b176dec2ba492966501ad
Eduqii <77701940+Eduqii@users.noreply.github.com>
2022-07-01 22:54:22 +0000

    Delete script.js

--- home/script.js
+++
@@ -1,7 +0,0 @@
-const donateNow = document.getElementById("donate-Now");
-
-donateNow.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    return window.location.assign("https://dashboard.flutterwave.com/donate/ul0fl3zqnpre");
-  }

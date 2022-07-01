b1e2a861ec4fb777a0d2db4d30394f84e3e9fa9a
Eduqii <77701940+Eduqii@users.noreply.github.com>
2022-07-01 23:15:26 +0000

    Update fscript.js

--- home/fscript.js
+++ home/fscript.js
@@ -1,7 +1,4 @@
-const donateNow = document.getElementById("donate-Now");
-
-donateNow.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    return window.location.assign("https://dashboard.flutterwave.com/donate/ul0fl3zqnpre");
+function donateNow(){
+    var page = window.location.assign("https://dashboard.flutterwave.com/donate/ul0fl3zqnpre");
+    return page;
   }

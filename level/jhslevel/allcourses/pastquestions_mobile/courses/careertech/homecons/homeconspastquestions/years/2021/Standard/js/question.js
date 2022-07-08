4c2da07686cbb2e76369cd6aa46d4a94dac9297c
Dwamena Bernard Hakeem <64783115+DwamenaHakim@users.noreply.github.com>
2022-07-08 12:08:22 +0000

    refactor

---
+++ level/jhslevel/allcourses/pastquestions_mobile/courses/careertech/homecons/homeconspastquestions/years/2021/Standard/js/question.js
@@ -0,0 +1,150 @@
+
+
+//Array of Objects
+const quiz = [
+  {
+  	question:'Seam include all these except.....',
+  	options:['Run and fell seam', 'French seam', 'Open seam', 'Loop switch'],
+  	answer:3
+  },
+  {	
+  	question: 'The fat soluble vitamins are....',
+		options:[ 'A,D,E and K vitamins','A and D vitamins', 'Vitamins A and K', 'Vitamins C, B and D'],
+		answer:1
+  },
+  {
+  	question:'The soil can be enriched before planting with....',
+  	options:['Flower', ' Seed',' Manure',' Plantain'],
+  	answer:2
+  },
+  {
+  	question:'The first thing to do when you want to clean a refrigerator is to.....',
+  	options:[ 'Switch on','Remove ice trays','Defrost','Remove its contents'],
+  	answer:2
+  },
+  {
+  	question:'In the kitchen arrangement, the sink should be near the window for....',
+  	options:[ 'Ventilation and walk','Light and ventilation','Light and walk','Size of cook'],
+  	answer:1
+  },
+  {
+  	question: 'The best method used in preserving food nutrients, while cooking is by....',
+		options: ['Frying','Baking','Steaming','Roasting'],
+		answer: 2
+  },
+  {
+  	question: '...... is a natural fibers that costs a lot.',
+	  options: [ 'Wool',' Silk','Rayon','Cotton'],
+		answer: 0
+  },
+  {
+  	question: 'A well groomed person has .....',
+		options: [ 'Well groomed hair','A neat and clean body','A neat and clean appearance','Well groomed hair and nails'],
+		answer: 2
+  },
+  {
+  	question: 'One of the following parts of the body is used for smelling',
+		options: ['Hands','Mouth','Tongue','Nose'],
+		answer: 3
+  },
+  {
+  	question: 'One of these is not a description of an aerobic exercise.',
+		options: ['Sit-ups','Cycling','Running','Jogging'],
+		answer: 0
+  },
+  {
+  	question: 'Exercise is a ........',
+		options: ['Mental movement','Body movements','Physical movement','Walking movement'],
+		answer: 2
+  },
+  {
+  	question: 'Stretching exercises are done to ......',
+		options: ['Develop muscles','Be healthy','Strengthen the heart','Firm the muscles'],
+		answer: 3
+  },
+  {
+  	question: 'Crossway strips are also called ......',
+		options: ['Binding strips','Warp','Bias','Weft'],
+		answer: 2
+  },
+  {
+  	question: 'The first step in the application of makeup is to.....',
+		options: ['Tone the face','Moisturise the face',' Cleanse the face','Powder the face'],
+		answer: 2
+  },
+  {
+  	question: 'The following are used in making up the face except.......',
+		options: ['Skin tonic','Face powder','Foundation cream','Cleansing lotion'],
+		answer: 3
+  },
+  {
+  	question: 'The provision of the basic family needs is the role of....',
+		options: ['Mother','Father, mother and children','Children','Father'],
+		answer: 3
+  },
+  {
+  	question: 'Which of the following is a transmitted disease?',
+		options: ['Hypertension','Gonorrhea','Headache','Hearing impairment'],
+		answer: 1
+  },
+  {
+  	question: 'Which of the following fibres is of natural source',
+		options: ['Acrylics','Polyester','Nylon','Silk'],
+		answer: 3
+  },
+  {
+  	question: 'Which of the following is not a floor covering?',
+		options: ['Terrazzo','Mat','Linoleum','Rug'],
+		answer: 0
+  },
+  {
+  	question: '..... is for healthy teeth?',
+		options: ['Flouride','Nutrient','Chloride','Protein'],
+		answer: 0
+  },
+  {
+  	question: 'Food stuffs that could be stored for a long period are called......',
+		options: ['Perishable','Non-protected','Protective','Non-perishable'],
+		answer: 3
+  },
+  {
+  	question: 'Your siblings son is your........',
+		options: ['Niece','Cousin','Nephew','Brother'],
+		answer: 2
+  },
+  {
+  	question: 'Needle used for embroidery are known as......',
+		options: ['Crewel','Betweens','Sewing needles','Darning needles'],
+		answer: 0
+  },
+  {
+  	question: 'The best methd of coooking an invalid dishes is......',
+		options: ['Boiling','Baking','Stewing','Steaming'],
+		answer: 3
+  },
+  {
+  	question: 'A broken marriage is also referred to as ......',
+		options: ['Engagement','Divorce','Crisis','Reunion'],
+		answer: 1
+  },
+  {
+  	question: 'Family needs are divided into ......',
+		options: ['Three','Five','Four','Two'],
+		answer: 3
+  },
+  {
+  	question: 'Which one can be tagged as a primary need?',
+		options: ['Housing','Car','Money','Telephone'],
+		answer: 0
+  },
+  {
+  	question: 'The secondary needs of a family include the following include the following except .......',
+		options: ['Education','Car','Food','Electricity'],
+		answer: 2
+  },
+  {
+  	question: 'The breaking down of food into smaller particles is called .......',
+		options: ['Metabolism','Digestion','Anabolism','Catabolism'],
+		answer: 1
+  }
+]
--- level/jhslevel/allcourses/pastquestions_mobile/courses/careertech/homecons/homeconspastquestions/years/homecons.js
+++ level/jhslevel/allcourses/pastquestions_mobile/courses/careertech/homecons/homeconspastquestions/years/homecons.js
@@ -1,5 +1,9 @@
-openWindowTwentyEleven = e => {
-        return window.location.assign('2020/index.php')
+quizTOne = e => {
+        return window.location.assign('2021/index.html')
+      }
+quizTwentyNine = e => {
+        alert("Very soon ranger");
+        // return window.location.assign('2020/index.php')
       }
 openWindowTwentyTwelve = e => {
         return window.location.assign('2019/index.php')

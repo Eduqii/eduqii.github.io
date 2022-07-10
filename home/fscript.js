2bbd837c5302651c06e78eb22b3a082c1f33268b
Dwamena Bernard Hakeem <64783115+DwamenaHakim@users.noreply.github.com>
2022-07-10 08:49:04 +0000

    refactor

--- home/fscript.js
+++ home/fscript.js
@@ -1,4 +1,18 @@
+
 function donateNow(){
     var page = window.open("https://flutterwave.com/donate/bjplsepb2trh");
     return page;
   }
+//logout
+  function logout(){
+    window.setTimeout(function() {
+            document.write("You are currently logged out \n redirecting to login page, please wait...");
+    window.location.href = 'https://eduqii.github.io/signin/index.html';
+}, 3000);
+  }
+//prevent page from moving back
+function preventBack() { 
+            window.history.forward(); 
+        }
+        setTimeout("preventBack()", 0);
+        window.onunload = function () { null };

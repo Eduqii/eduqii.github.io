73c06f18df18bb5a40e6d02cb4f8195dc4c1829c
Dwamena Bernard Hakeem <64783115+DwamenaHakim@users.noreply.github.com>
2022-07-12 07:15:14 +0000

    refactor

--- signin/signin.js
+++ signin/signin.js
@@ -9,17 +9,8 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
 
-        if(level === "jhs" && email === "SirSammy@username" || email== "sirsammy@username" || email=="SIRSAMMY@USERNAME" && password === "100%1"){
-            var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-            document.write("Logging in as Sir Sammy. Please wait...")
-            if (isMobile) {
-              window.location.assign("https://eduqii.github.io/level/jhslevel/allcourses/pastquestions_mobile/index.html");
-            }
-            else{
-              window.location.assign("https://eduqii.github.io/level/jhslevel/allcourses/web_pastquestions/index.html");
-            }
-        }
-        else if(level === "jhs" && email ==="guestuser@me" && password ==="g100"){
+        
+        if(level === "jhs" && email ==="guestuser@me" && password ==="g100"){
             var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
             document.write("logging in as guest. Please wait...");
             if (isMobile) {
@@ -30,9 +21,6 @@ loginButton.addEventListener("click", (e) => {
             }
         }
         else if (level == "primary" && email ==="guestuser@me" && password ==="g100") {
-            return window.location.assign("https://eduqii.github.io/level/primarylevel/index.html");
-        }
-        else if (level == "primary" && email === "SirSammy@username" || email== "sirsammy@username" || email=="SIRSAMMY@USERNAME" && password === "100%1") {
             return window.location.assign("https://eduqii.github.io/level/primarylevel/index.html");
         }
 
